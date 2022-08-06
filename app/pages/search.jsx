626feba242010a2cea3ffd0f85ce2bb4aaddcc52
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-08-06 21:18:37 +0200

    test2 correct height

--- app/pages/search.jsx
+++ app/pages/search.jsx
@@ -63,7 +63,7 @@ export default function Search({ user }) {
 
     const handleOnUsersTagClick = () => setTag('users')
 
-    return <div className="flex flex-col h-[-webkit-fill-available] h-[-moz-available] h-[fill-available]">
+    return <div className="flex flex-col h-[-webkit-fill-available] h-[-moz-available] h-[stretch]">
         <Header className="pb-2" title="Search" />
         <FlexColSection className="py-4 flex-1 overflow-y-auto items-center gap-4" >
 
