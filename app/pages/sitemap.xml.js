62833caa4d27edbb622af9f1a485d405c25fcb12
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-08-08 10:17:05 +0200

    add sitemap

--- app/pages/sitemap.xml.js
+++ app/pages/sitemap.xml.js
@@ -8,7 +8,7 @@ export async function getServerSideProps({ res }) {
     const baseUrl = 'https://pitch-us.vercel.app'
 
         const staticPages = fs
-            .readdirSync('./')
+            .readdirSync('./pages')
             .filter((staticPage) => {
                 return ![
                     '_app.jsx',
