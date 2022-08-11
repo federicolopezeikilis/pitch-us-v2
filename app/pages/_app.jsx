77413901fc052d5ad394ba926a3b9e716fc6d1c2
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-08-11 11:55:36 +0200

    random

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Head from 'next/head'
-import { AppWrapper } from '../components';
+import { AppWrapper } from '../components'
+import { Provider } from 'next-auth/client'
 
 if (typeof XMLHttpRequest === 'undefined') {
   var XMLHttpRequest = require('xhr2');
@@ -14,11 +15,13 @@ function MyApp({ Component, pageProps }) {
       <title>PitchUs</title>
       <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
     </Head>
-    <div className="box-border w-full h-full flex flex-col bg-white">
-      <AppWrapper>
-        <Component {...pageProps} />
-      </AppWrapper>
-    </div>
+    <Provider session={pageProps.session}>
+      <div className="box-border w-full h-full flex flex-col bg-white">
+        <AppWrapper>
+          <Component {...pageProps} />
+        </AppWrapper>
+      </div>
+    </Provider>
   </>
 }
 
