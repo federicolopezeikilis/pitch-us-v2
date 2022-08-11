8d666a18c828521733bc76354b629ecac6839ac2
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-08-11 11:53:46 +0200

    revert merge

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import Head from 'next/head'
-import { AppWrapper } from '../components'
-import { Provider } from 'next-auth/client'
+import { AppWrapper } from '../components';
 
 if (typeof XMLHttpRequest === 'undefined') {
   var XMLHttpRequest = require('xhr2');
@@ -15,13 +14,11 @@ function MyApp({ Component, pageProps }) {
       <title>PitchUs</title>
       <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
     </Head>
-    <Provider session={pageProps.session}>
-      <div className="box-border w-full h-full flex flex-col bg-white">
-        <AppWrapper>
-          <Component {...pageProps} />
-        </AppWrapper>
-      </div>
-    </Provider>
+    <div className="box-border w-full h-full flex flex-col bg-white">
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
+    </div>
   </>
 }
 
